71f3fa53a606aed9031c09251ee56afdcf5645f2
NilsHellwig <nc.hellwig@gmail.com>
2022-12-11 15:25:56 +0100

    create new method

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@ function addEditListener(taskEditElement, taskInputElement) {
       // he "readonly" attribute is deleted by this action
       taskInputElement.focus();
       taskEditElement.innerText = "Speichern";
-      isInEditMode = true
+      isInEditMode = true;
     } else {
       taskInputElement.setAttribute("readonly", "readonly");
       taskEditElement.innerText = "Bearbeiten";
-      isInEditMode = false
+      isInEditMode = false;
     }
   });
 }
@@ -37,6 +37,41 @@ function addActionButtons(taskActionsElement, taskEditElement, taskDeleteElement
   taskActionsElement.appendChild(taskDeleteElement);
 }
 
+function createNewTask() {
+  // The document.createElement() method is used to create a new element in an HTML document.
+  // It takes a single argument, which is the name of the element to be created, and returns a reference to the new element.
+  const taskElement = document.createElement("div");
+  taskElement.classList.add("task");
+
+  const taskContentElement = document.createElement("div");
+  taskContentElement.classList.add("content");
+
+  const taskInputElement = document.createElement("input");
+  taskInputElement.classList.add("text");
+  taskInputElement.type = "text";
+  taskInputElement.value = task;
+
+  // The task_input_el.setAttribute("readonly", "readonly") method sets the readonly attribute on the task_input_el element.
+  // This means that the element will be displayed, but the user will not be able to interact with it or modify its content.
+  taskInputElement.setAttribute("readonly", "readonly");
+
+  // Add action buttons
+  const taskActionsElement = document.createElement("div");
+  taskActionsElement.classList.add("actions");
+
+  const taskEditElement = document.createElement("button");
+  const taskDeleteElement = document.createElement("button");
+
+  addActionButtons(taskActionsElement, taskEditElement, taskDeleteElement);
+  addEditListener(taskEditElement, taskInputElement);
+  addDeleteListener(taskDeleteElement, taskElement);
+
+  taskElement.appendChild(taskContentElement);
+  taskContentElement.appendChild(taskInputElement);
+  taskElement.appendChild(taskActionsElement);
+  listElement.appendChild(taskElement);
+}
+
 form.addEventListener("submit", (e) => {
   // When using the e.preventDefault(); method in a submit event, it prevents the default
   // action of the submit event from happening. In the case of a form submit event,
@@ -46,39 +81,7 @@ form.addEventListener("submit", (e) => {
   if (!task) {
     alert("Bitte gebe einen Task ein!");
   } else {
-    // The document.createElement() method is used to create a new element in an HTML document.
-    // It takes a single argument, which is the name of the element to be created, and returns a reference to the new element.
-    const taskElement = document.createElement("div");
-    taskElement.classList.add("task");
-
-    const taskContentElement = document.createElement("div");
-    taskContentElement.classList.add("content");
-
-    const taskInputElement = document.createElement("input");
-    taskInputElement.classList.add("text");
-    taskInputElement.type = "text";
-    taskInputElement.value = task;
-
-    // The task_input_el.setAttribute("readonly", "readonly") method sets the readonly attribute on the task_input_el element.
-    // This means that the element will be displayed, but the user will not be able to interact with it or modify its content.
-    taskInputElement.setAttribute("readonly", "readonly");
-
-    // Add action buttons
-    const taskActionsElement = document.createElement("div");
-    taskActionsElement.classList.add("actions");
-
-    const taskEditElement = document.createElement("button");
-    const taskDeleteElement = document.createElement("button");
-
-    addActionButtons(taskActionsElement, taskEditElement, taskDeleteElement);
-    addEditListener(taskEditElement, taskInputElement);
-    addDeleteListener(taskDeleteElement, taskElement);
-
-    taskElement.appendChild(taskContentElement);
-    taskContentElement.appendChild(taskInputElement);
-    taskElement.appendChild(taskActionsElement);
-    listElement.appendChild(taskElement);
-
+    createNewTask(task);
     // Clear text input
     input.value = "";
   }
