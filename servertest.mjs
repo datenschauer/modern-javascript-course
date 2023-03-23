c33b2d59b01b8833ca808ce20dd78a0dae7bd58e
Stefan Boehringer <stefan.boehringer@paedagogik.uni-regensburg.de>
2023-03-23 15:14:52 +0100

    send a html page

--- servertest.mjs
+++ servertest.mjs
@@ -1,11 +1,22 @@
 import { createServer } from 'http';
 
 const server = createServer((req, res) => {
-    res.writeHead(200, {
-        'content-type': 'text/plain; charset=utf-8;',
+    res.writeHead(200, { // Senden eines gültigen HTTP Headers
+        'content-type': 'text/html; charset=utf-8;', // content als html deklarieren
     });
-    res.write('Hallo Besucher! Willkommen!');
-    res.end('');
+    // eine html Seite definieren
+    const body = `<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Node.js Demo</title>
+        </head>
+        <body>
+            <h1 style="color: red">Willkommen!</h1>
+        </body>
+    </html>`
+
+    res.end(body); // dem Client das HTML direkt senden
 });
 
 server.listen(8080, "127.0.0.1", () => {
