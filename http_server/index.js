c49a13829f79a78ce40f78d76986b14fcb74d61d
Stefan Boehringer <stefan.boehringer@paedagogik.uni-regensburg.de>
2023-04-18 10:16:09 +0200

    Hinzufügung des Adressbuchs

--- http_server/index.js
+++ http_server/index.js
@@ -1,12 +1,46 @@
 import { createServer } from 'http';
 
+// wir legen uns ein paar Adressen an
+const addresses = [
+    {
+    id: 1,
+    firstname: "Stefan",
+    lastname: "Böhringer",
+    street: "Mainstr. 4",
+    city: "Regensburg",
+    },
+    {
+     id: 2,
+     firstname: "Nils",
+     lastname: "Hellwig",
+     street: "Donauweg 12",
+     city: "Regensburg",
+    }
+]
+
 // unsere html Seite, die vom Server zurückgegeben wird
 const body = `
 <!DOCTYPE html>
-<html>
-<head><title>Adressbuch</title></head>
+<html lang="de">
+<head>
+<meta charset="UTF-8">
+<title>Adressbuch</title>
+</head>
 <body>
 <h1>Mein Adressbuch</h1>
+<table>
+<thead>
+    <tr>
+    <th>Vorname</th>
+    <th>Nachname</th>
+    <th>Strasse</th>
+    <th>Stadt</th>
+    </tr>
+</thead>
+<tbody>
+    ${addresses.map(createRow).join('')}
+</tbody>
+</table>
 </body>
 </html>
 `
@@ -24,3 +58,14 @@ const server = createServer((req, res) => {
 server.listen(8888, () => {
     console.log('Adressbuch erreichbar unter http://localhost:8888');
 })
+
+// erzeugt eine Reihe unseres Adressbuchs
+function createRow(address) {
+    return `<tr>
+        <td>${address.firstname}</td>
+        <td>${address.lastname}</td>
+        <td>${address.street}</td>
+        <td>${address.city}</td>
+        </tr>
+        `;
+}
