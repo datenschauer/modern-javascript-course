c2f5f8061145afe291c97480e4b0c569722803bf
Stefan Boehringer <stefan.boehringer@paedagogik.uni-regensburg.de>
2023-04-19 11:54:14 +0200

    initiale Adressdaten gelöscht, da diese nicht mehr gebraucht werden

--- http_server/data.js
+++ http_server/data.js
@@ -1,32 +1,6 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 import path from 'node:path';
 
-export default {
-    addresses: [
-        {
-            id: 1,
-            firstname: "Stefan",
-            lastname: "Böhringer",
-            street: "Mainstr. 4",
-            city: "Regensburg",
-        },
-        {
-            id: 2,
-            firstname: "Nils",
-            lastname: "Hellwig",
-            street: "Donauweg 12",
-            city: "Regensburg",
-        },
-        {
-            id: 3,
-            firstname: "Angela",
-            lastname: "Merkel",
-            street: "Kanzlerstr. 66",
-            city: "Hauptstadt",
-        }
-    ],
-};
-
 const DATA_FILE = path.resolve('data', 'addresses.json');
 
 export function getData() {
