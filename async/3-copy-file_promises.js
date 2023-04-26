588cc7a8f64df27835b2515c4217cbecec03f530
Stefan Boehringer <stefan.boehringer@paedagogik.uni-regensburg.de>
2023-04-26 10:59:14 +0200

    copy file Funktion mit Promises gelöst

---
+++ async/3-copy-file_promises.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import fs from 'fs';
+
+const readInput = function (fileName) {
+    // wir geben ein Promise zurück, dass mit einem Callback aufgerufen wird
+    // hier ist zwar immer noch ein Callback involviert, aber der weitere Code
+    // sollte deutlich einfacher werden (?)
+    return new Promise((resolve, reject) => {
+        fs.readFile(fileName, { encoding: 'utf8'}, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(data);
+        })
+    });
+};
+
+const writeOutput = function (fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, { encoding: 'utf8' }, err => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        })
+    })
+};
+
+const copy = function (source, target) {
+    // auch diese Funktion muss asynchron sein, also ein Promise zurückgeben:
+    return new Promise((resolve, reject) => {
+        // Wir wissen nun, dass unsere Funktionen readInput und writeOutput Promises zurückgeben.
+        // Jedes Promise Objekt hat eine Methode .then() und eine Methode .catch(), die wir verwenden
+        // können, um zu sagen: wenn etwas zurückgegeben wird DANN mache X, ANDERENFALLS mache Y.
+        readInput(source)
+            // leider nehmen then() und catch() wieder callbacks entgegen :-/
+            .then(data => {
+                writeOutput(target, data)
+                    .then(() => {
+                        resolve();
+                    })
+                    .catch(err => {
+                        reject(err);
+                    });
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+};
+
+// wir testen das Ganze!
+copy('input.txt', 'output.txt')
+    .then(() => {
+        console.log('Datei kopiert!');
+    })
+    .catch(err => {
+        console.log(err);
+    });
