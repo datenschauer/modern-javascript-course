36de5c3db49c24bed48ba1a983baefe0817830ab
Stefan Boehringer <stefan.boehringer@paedagogik.uni-regensburg.de>
2023-04-26 12:25:55 +0200

    überflüssigen Catch-Block loswerden

--- async/6-copy-file_promise-api.js
+++ async/6-copy-file_promise-api.js
@@ -3,16 +3,12 @@
 import fs from 'fs/promises';
 
 const copy = async function (source, target) {
-    try {
-        const data = await fs.readFile(source, { encoding: 'utf8' });
-        await fs.writeFile(target, data, { encoding: 'utf8' });
-    } catch (ex) {
-        throw ex; // eigentlich überflüssig (verdeutlicht nur die Verwendung von try-catch)!
-    }
+    const data = await fs.readFile(source, { encoding: 'utf8' });
+    await fs.writeFile(target, data, { encoding: 'utf8' });
 };
 
 try {
-    await copy('input.txt', 'output.txt');
+    await copy('input3.txt', 'output.txt');
     console.log('Datei kopiert!');
 } catch (ex) {
     console.log(ex);
