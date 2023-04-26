9a7a49fd2df8890f39261145e52e40abe7fd928b
Stefan Boehringer <stefan.boehringer@paedagogik.uni-regensburg.de>
2023-04-26 08:39:13 +0200

    setTimeout auf 0 trotzdem am Ende

--- async/async-test.js
+++ async/async-test.js
@@ -4,8 +4,9 @@ console.log('Start');
 
 console.log('Wait for it... ');
 
+// Warum läuft der Aufruf von 'Hallo Welt' immer noch am Ende, selbst wenn wir den Timer auf 0 setzen?
 setTimeout(() => {
     console.log('Hallo Welt!');
-}, 3_000);
+}, 0);
 
 console.log('Ende');
